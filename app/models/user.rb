6f03f2a24ba4e791f1b4e623d0f72071cfb609da
K-Akira928 <K.AkiraRei928@gmail.com>
2024-02-17 20:41:11 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+
+  validates :name, presence: true, length: { maximum: 16 }, uniqueness: true
+  validates :nickname, presence: true
+  validates :phone, presence: true, format: { with: /\A[0-9]+\z/ }
+  validates :birthday, presence: true
+
   include DeviseTokenAuth::Concerns::User
 end
