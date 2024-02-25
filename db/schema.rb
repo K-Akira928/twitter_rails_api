85538eea0eea397f6d267d461eecc43d16f0acea
K-Akira928 <K.AkiraRei928@gmail.com>
2024-02-25 15:13:27 +0900

    model Tweet を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_12_122011) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_25_054534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_12_122011) do
     t.string "title", null: false
     t.text "description", null: false
     t.integer "status", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
