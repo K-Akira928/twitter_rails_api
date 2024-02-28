137cc7660da89864ebd9ad6aa0a1f300c0732011
K-Akira928 <K.AkiraRei928@gmail.com>
2024-02-29 01:41:22 +0900

    rubocopで修正

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,18 +1,24 @@
-class Api::V1::ImagesController < ApplicationController
-  def update
-    tweet = current_api_v1_user.tweets.find(params[:id])
-    if tweet.update(image_params)
-      render json: { status: :updated },
-             status: 200
-    else
-      render json: { status: :unprocessable_entity, errros: tweet.errors },
-             status: 422
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ImagesController < ApplicationController
+      def update
+        tweet = current_api_v1_user.tweets.find(params[:id])
+        if tweet.update(image_params)
+          render json: { status: :updated },
+                 status: :ok
+        else
+          render json: { status: :unprocessable_entity, errros: tweet.errors },
+                 status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def image_params
+        params.require(:tweet).permit(images: [])
+      end
     end
-  end
-
-  private
-
-  def image_params
-    params.require(:tweet).permit(images: [])
   end
 end
