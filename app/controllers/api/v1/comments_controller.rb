32089e97d884de62d0399d34e64583809c7f7570
K-Akira928 <K.AkiraRei928@gmail.com>
2024-03-10 20:33:43 +0900

    コメント作成apiを実装

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def create
+        comment = Comment.new(parent_tweet_id: params[:parent_tweet_id], comment_tweet_id: params[:comment_tweet_id])
+        if comment.save
+          render json: { comment: },
+                 status: :ok
+        else
+          render json: { message: 'コメントの作成に失敗しました' },
+                 status: :bad_request
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
         resources :users, only: %i[index]
       end
       resources :tweets, only: %i[index show create destroy]
+      resources :comments, only: %i[create]
       resources :images, only: %i[update]
       mount_devise_token_auth_for 'User', at: 'users', controllers: {
         registrations: 'api/v1/users/registrations'
