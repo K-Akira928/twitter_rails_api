384fcb33e53445d3296dd0c3b0c2a8e562f7e3d7
K-Akira928 <K.AkiraRei928@gmail.com>
2024-03-19 10:02:04 +0900

    model NoticeTypeを作成

---
+++ db/migrate/20240319005942_create_notice_types.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateNoticeTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notice_types do |t|
+      t.string :notice_name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_16_121843) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_19_005942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_16_121843) do
     t.datetime "updated_at", null: false
     t.index ["follow_user_id"], name: "index_follows_on_follow_user_id"
     t.index ["follower_user_id"], name: "index_follows_on_follower_user_id"
+  end
+
+  create_table "notice_types", force: :cascade do |t|
+    t.string "notice_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "retweets", force: :cascade do |t|
